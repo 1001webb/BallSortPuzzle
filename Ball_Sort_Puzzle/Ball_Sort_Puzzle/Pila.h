1bdb2649fce5499a19ecc64673fd207cc3a90939
1001webb <69720765+1001webb@users.noreply.github.com>
2020-08-16 19:51:50 -0600

    commit prueba

---
+++ Ball_Sort_Puzzle/Ball_Sort_Puzzle/Pila.h
@@ -0,0 +1,11 @@
+#include <iostream>
+#include <cstdlib>
+
+
+struct pila {
+	Bola pelota;
+	struct Pila* sig;
+}*primero=NULL;
+
+void 
+
--- Ball_Sort_Puzzle/Ball_Sort_Puzzle/main.cpp
+++ Ball_Sort_Puzzle/Ball_Sort_Puzzle/main.cpp
@@ -1,3 +1,34 @@
-int main() {
-	return 0;
+#include <iostream>
+#include <cstdlib>
+#include "SFML/Graphics.hpp"
+
+using namespace sf;
+
+class Bola {
+
+};
+
+struct pila {
+	Bola pelota;
+	struct pila* sig;
+}*primero = NULL;
+
+void insertarBola(Bola pelota) 
+{
+	pila *nuevo = (pila*)malloc(sizeof(pila));
+	nuevo->pelota = pelota;
+	nuevo->sig = primero;
+	primero = nuevo;
 }
+
+class Tubo_Ensayo
+{
+private:
+	Texture *txtT;
+	Sprite* sptT;
+public:
+	void dibujarTubo()
+	{
+
+	}
+};
